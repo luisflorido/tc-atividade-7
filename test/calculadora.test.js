2aabd443e84bfc7b835a1258086132fd8b583440
Luis <luiisflorido@gmail.com>
2021-03-17 03:45:11 -0300

    feat: adiciona teste dba salario maior que 2000

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -41,4 +41,19 @@ describe('Testa classe calculadora de salário', () => {
         const calcSalario = calculadora.calcularSalario(funcionario)
         expect(calcSalario).toBe(1800)
     })
+
+    describe('DBA salario maior que 2000', () => {
+        const funcionario = {
+            nome: 'Teste',
+            email: 'teste@gmail.com',
+            salarioBase: 3000,
+            cargo: 'DBA',
+        }
+        const errors = funcionarioSchema.validate(funcionario);
+
+        expect(errors).toHaveLength(0);
+
+        const calcSalario = calculadora.calcularSalario(funcionario)
+        expect(calcSalario).toBe(2250)
+    })
 })
