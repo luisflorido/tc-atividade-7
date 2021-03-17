5e510a25a762024b8e6d13c9b36eeadb532c8ea0
Luis <luiisflorido@gmail.com>
2021-03-17 03:42:49 -0300

    feat: adiciona teste dev salario maior que 3000

---
+++ test/calculadora.test.js
@@ -0,0 +1,30 @@
+const Calculadora = require('../lib/calculadora');
+const Schema = require('validate');
+
+const calculadora = new Calculadora();
+
+const funcionarioSchema = new Schema({
+    nome: String,
+    email: String,
+    salarioBase: Number,
+    cargo: String
+})
+
+describe('Testa classe calculadora de salário', () => {
+    
+    describe('DESENVOLVEDOR salario maior que 3000', () => {
+        const funcionario = {
+            nome: 'Teste',
+            email: 'teste@gmail.com',
+            salarioBase: 4000,
+            cargo: 'DESENVOLVEDOR',
+        }
+        const errors = funcionarioSchema.validate(funcionario);
+
+        expect(errors).toHaveLength(0);
+
+        const calcSalario = calculadora.calcularSalario(funcionario)
+        expect(calcSalario).toBe(3200)
+    })
+
+})
